720b7ad962da3225e087eef371ecce21739a40d3
andrew-paterson <andrew.paterson9@gmail.com>
2021-02-10 17:36:29 +0200

    Import issue.

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,8 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 
-  model() {
-    return this.store.findRecord('user', 1);
-  }
+  // model() {
+  //   return this.store.findRecord('user', 1);
+  // }
 
 });
--- mirage/config.js
+++ mirage/config.js
@@ -1,3 +1,5 @@
+import mockResponse from  'test-addon-2/test-support/mock-data';
+
 export default function() {
 
   this.resource('user')
